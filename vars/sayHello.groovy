39e4f03fb121edce2c0da8a5a2b5a242a8407443
omair-khan-roostify <42222955+omair-khan-roostify@users.noreply.github.com>
2018-08-20 18:01:12 -0700

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,10 +1,18 @@
 #!/usr/bin/env groovy
 
 def call(String name = '.') {
+ pipeline 
+  {
+    agent any
+
+    environment {
+            AWS_ECR_LOGIN='true'
+    }
 stage ('test') {
             steps {
                 sh "./sonarqube-scanner-gradle/gradlew -b ./sonarqube-scanner-gradle/build.gradle test"
             }                
         }
+}
 }
 
